2ca2d6b2625fd7840938110a89d7b0623a8d0e6b
Andres Rivero <andres19rivero@gmail.com>
2023-11-19 22:06:07 -0400

    feat: create shared module & generate customLabel directive

---
+++ src/app/shared/directives/custom-label.directive.ts
@@ -0,0 +1,15 @@
+import { Directive, ElementRef } from '@angular/core';
+
+@Directive({
+  selector: '[customLabel]',
+})
+export class CustomLabelDirective {
+  private htmlElement?: ElementRef<HTMLElement>;
+
+  constructor(private el: ElementRef<HTMLElement>) {
+    console.log(el);
+    this.htmlElement = el;
+
+    this.htmlElement.nativeElement.innerHTML = 'Hello World';
+  }
+}
