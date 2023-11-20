c22ebbad6447eabc8f9602db6ebbae9f0e0e21a9
Andres Rivero <andres19rivero@gmail.com>
2023-11-19 22:35:52 -0400

    feat(customLabel): change error message in html

--- src/app/shared/directives/custom-label.directive.ts
+++ src/app/shared/directives/custom-label.directive.ts
@@ -1,4 +1,5 @@
 import { Directive, ElementRef, Input, OnInit } from '@angular/core';
+import { ValidationErrors } from '@angular/forms';
 
 @Directive({
   selector: '[customLabel]',
@@ -6,14 +7,19 @@ import { Directive, ElementRef, Input, OnInit } from '@angular/core';
 export class CustomLabelDirective implements OnInit {
   private htmlElement?: ElementRef<HTMLElement>;
   private _color: string = 'red';
+  private _errors?: ValidationErrors | null;
 
   @Input() set color(value: string) {
     this._color = value;
     this.setStyle();
   }
 
+  @Input() set errors(value: ValidationErrors | null | undefined) {
+    this._errors = value;
+    this.setErrorMessage();
+  }
+
   constructor(private el: ElementRef<HTMLElement>) {
-    // console.log(el);
     this.htmlElement = el;
   }
 
@@ -24,5 +30,29 @@ export class CustomLabelDirective implements OnInit {
   setStyle(): void {
     if (!this.htmlElement) return;
     this.htmlElement!.nativeElement.style.color = this._color;
+  }
+
+  setErrorMessage(): void {
+    if (!this.htmlElement) return;
+    if (!this._errors) {
+      this.htmlElement.nativeElement.innerHTML = 'There is no errors';
+      return;
+    }
+
+    const errors = Object.keys(this._errors);
+    if (errors.includes('required')) {
+      this.htmlElement.nativeElement.innerHTML = 'This field is required';
+      return;
+    }
+    if (errors.includes('minlength')) {
+      const min = this._errors!['minlength']['requiredLength'];
+      const current = this._errors!['minlength']['actualLength'];
+      this.htmlElement.nativeElement.innerHTML = `Min ${current}/${min} characters`;
+      return;
+    }
+    if (errors.includes('email')) {
+      this.htmlElement.nativeElement.innerHTML = 'No email format';
+      return;
+    }
   }
 }
