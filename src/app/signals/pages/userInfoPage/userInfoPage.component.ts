f00c2df761bd4592b4430ee501907adbf6f75a14
Andres Rivero <andres19rivero@gmail.com>
2023-11-29 22:54:05 -0400

    feat(user-info): call to service to get users & show them in template

--- src/app/signals/pages/userInfoPage/userInfoPage.component.ts
+++ src/app/signals/pages/userInfoPage/userInfoPage.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+import { UsersService } from '../../services/users.service';
+import { User } from '../../interfaces/user-request.interface';
 
 @Component({
   selector: 'app-userInfoPage',
   templateUrl: './userInfoPage.component.html',
-  styleUrls: ['./userInfoPage.component.css']
+  styleUrls: ['./userInfoPage.component.css'],
 })
 export class UserInfoPageComponent implements OnInit {
+  public userId = signal(1);
+  public currentUser = signal<User | undefined>(undefined);
+  public userWasFound = signal(true);
 
-  constructor() { }
+  constructor(private userService: UsersService) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.loadUser(this.userId());
   }
 
+  loadUser(id: number) {
+    if (id <= 0) return;
+
+    this.userId.set(id);
+    this.currentUser.set(undefined);
+    this.userService.getUserById(id).subscribe((user) => {
+      this.currentUser.set(user);
+    });
+  }
 }
